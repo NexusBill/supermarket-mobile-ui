8145a8e2baf97a453078cafdece0770ed54546b7
NexusBill <nexusbills.official@gmail.com>
2026-01-17 12:21:35 +0530

    id change

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.kaviyarasan.kamatchisupermarket',
   appName: 'Kamatchi supermart',
   webDir: 'www',
   fullscreen: false
